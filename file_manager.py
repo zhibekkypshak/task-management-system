a94aa57f60a07ff6ca18345d1a9b9bb12b03e66c
neobchambers <totallynotsatana@gmail.com>
2026-05-22 02:27:05 +0500

    Update file_manager.py
    created save_to_file function(try/except)

--- file_manager.py
+++ file_manager.py
@@ -4,3 +4,12 @@ import os
 class FileManager:
     def __init__(self, filepath="data/tasks.json"):
         self.filepath = filepath
+        
+    def save_to_file(self, tasks):
+        try:
+            os.makedirs(os.path.dirname(self.filepath), exist_ok=True)
+            with open(self.filepath, "w", encoding="utf-8") as f:
+                json.dump(tasks, f, indent=4, ensure_ascii=False)
+            print("Tasks saved successfully")
+        except IOError as e:
+            print(f"Error saving tasks: {e}")
