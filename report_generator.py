cc92b9997322253f0431b7a89267a2b88430ecb1
zhibekkypshak <zibekkypsak123@gmail.com>
2026-05-21 22:09:42 +0500

    Improved report  logic

--- report_generator.py
+++ report_generator.py
@@ -11,21 +11,21 @@ class ReportGenerator:
         if completed_tasks:
             for task in completed_tasks:
                 print(task)
-            else:
-                print("No completed tasks")
+        else:
+            print("No completed tasks")
 
     def generate_priority_task_report(self):
         high=0
         medium=0
         low=0
         for task in self.tasks:
-            if task["status"]=="high":
+            if task["priority"]=="high":
                 high+=1
-            elif task["status"]=="medium":
+            elif task["priority"]=="medium":
                 medium+=1
-            elif task["status"]=="low":
+            elif task["priority"]=="low":
                 low+=1
-        print("\nHigh priority tasks report:")
+        print("\nPriority tasks report:")
         print("High priority tasks:"+str(high))
         print("Medium priority tasks:"+str(medium))
         print("Low priority tasks:"+str(low))
@@ -39,8 +39,8 @@ class ReportGenerator:
         if pending_tasks:
             for task in pending_tasks:
                 print(task)
-            else:
-                print("No pending tasks")
+        else:
+            print("No pending tasks")
 
     def generate_in_progress_report(self):
         in_progress_tasks=[
@@ -51,5 +51,5 @@ class ReportGenerator:
         if in_progress_tasks:
             for task in in_progress_tasks:
                 print(task)
-            else:
-                print("No in-progress tasks")
+        else:
+            print("No in-progress tasks")
