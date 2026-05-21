f72153e4eb5cecd2c1baaab727e6830aa2d1b57c
zhibekkypshak <zibekkypsak123@gmail.com>
2026-05-21 21:54:19 +0500

    Added completed tasks report

--- report_generator.py
+++ report_generator.py
@@ -1,3 +1,16 @@
 class ReportGenerator:
     def __init__(self, tasks):
         self.tasks=tasks
+
+    def generate_completed_report(self):
+        completed_tasks=[
+            task for task in self.tasks
+            if task["status"]=="completed"
+        ]
+        print("\nCompleted tasks report:")
+        if completed_tasks:
+            for task in completed_tasks:
+                print(task)
+            else:
+                print("No completed tasks")
+
